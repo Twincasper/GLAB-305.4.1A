f04e38803e769a335279a08fad13a4a9af3fdcc7
Twincasper <justinwatson651@gmail.com>
2024-12-17 15:21:12 -0500

    Updated gitignore and hibernate configuration

--- src/main/java/com/test/project/HibernateJPABeginner/App.java
+++ src/main/java/com/test/project/HibernateJPABeginner/App.java
@@ -1,13 +1,18 @@
 package com.test.project.HibernateJPABeginner;
 
-/**
- * Hello world!
- *
- */
+import com.test.hib.controller.findUser_Hql;
+
 public class App 
-{
-    public static void main( String[] args )
+{ 
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	findUser_Hql u = new findUser_Hql();
+     // u.findUser();
+     // u.findUserSelect();
+     // u.getRecordbyId();
+     //	u.getrecords();
+     //	u.getmaxSalary();
+     //	u.getmaxSalaryGroupBy();
+    	u.NamedQueryExample();
     }
 }
